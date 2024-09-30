9cb45a33f9b4ca437f5cc064ec3f483a6da7204b
Yizhou Mi <miyizhou@remote.ecf.utoronto.ca>
2024-09-30 16:40:53 -0400

    brief completioon of Node and Resistor

--- lab3_release/Node.cpp
+++ lab3_release/Node.cpp
@@ -1,5 +1,47 @@
 //
 //  Node.cpp
 //  Lab 3 The Resistor Network Program
-// Author: 
-// Date:
+// Author: Joseph Mi
+// Date: Sept 30, 2024
+
+#include <iostream>
+#include "Node.h"
+#include "string.h"
+
+bool Node::canAddResistor() {
+    return numRes < MAX_RESISTORS_PER_NODE;
+}
+
+void Node::addResistor(int rIndex) {
+    if (canAddResistor()) {
+        numRes += 1;
+        resIDArray.append(rIndex);
+    }
+}
+
+double Node::getVoltage() {
+    return voltage;
+}
+
+void Node::setVoltage(double voltage_) {
+    voltage = voltage_;
+    voltageIsSet = true;
+}
+
+// void Node::setVoltageInternally(double voltage_) {
+//     if () {
+
+//     }
+// }
+
+bool Node::isVoltageSet() {
+    return voltageIsSet;
+}
+
+int Node::getNumRes() {
+    return numRes;
+}
+
+int* Node::getResIDArray() {
+    return resIDArray;
+}
